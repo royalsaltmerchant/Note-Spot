7f8a927e9ee61f2627ed702358d5952609bda49c
Julian ranieri <jumpingafterrain@gmail.com>
2022-05-09 13:01:14 -0600

    fix bugs

--- note.js
+++ note.js
@@ -1,60 +1,69 @@
-  var noteCount = 1
+var noteCount = 1
 
-  window.getNote = function(elem) {
-    var id = elem.closest('.note-block').id
-    return window.notes[id]
-  }
-  
-  // dynamic render of note component
-  function Note(props) {
-    this.id = props.id
-    this.title = ''
-    this.text = ''
+window.getNote = function(elem) {
+  var id = elem.closest('section').id
+  return window.notes[id]
+}
 
-    var self = this
+// dynamic render of note component
+function Note(props) {
+  this.id = props.id
+  this.title = ''
+  this.text = ''
 
-    // store a hash of notes
-    if(!window.notes) window.notes = {}
-    window.notes[this.id] = this
+  var self = this
 
-    this.render = function() {
-      var section = document.createElement('section')
-      section.id = this.id
-      section.classList.add('resize')
-      section.addEventListener('mousedown', function(e) {
-        e.stopPropagation()
-        e.preventDefault()
-        var notesArray = Object.values(window.notes)
-        notesArray.forEach(function(note) {
-          var noteElem = document.getElementById(note.id)
-          if(section.id === noteElem.id) {
-            section.style.zIndex = 2
-          } else noteElem.style.zIndex = 1
-        })
-      }, true)
-      
-      var sectionBody = /*html*/ `
+  // store a hash of notes
+  if(!window.notes) window.notes = {}
+  window.notes[this.id] = this
+
+  this.render = function() {
+    var section = document.createElement('section')
+    section.id = this.id
+    section.classList.add('resize')
+    section.addEventListener('mousedown', function(e) {
+      var notesArray = Object.values(window.notes)
+      notesArray.forEach(function(note) {
+        var noteElem = document.getElementById(note.id)
+        if(section.id === noteElem.id) {
+          section.style.zIndex = 2
+        } else noteElem.style.zIndex = 1
+      })
+    }, true)
+    
+    var sectionBody = /*html*/ `
       <div class="draggable-header">
-      <input onfocusout="var self = window.getNote(this); self.title = this.value;" id="title" placeholder="New Note">
-      <button onclick="var self = window.getNote(this); self.removeNote(this.parentElement.parentElement)">✕</button>
+        <input onfocusout="var self = window.getNote(this); self.title = this.value;" id="title" placeholder="New Note">
+        <button onclick="var self = window.getNote(this); self.removeNote(this.parentElement.parentElement)">✕</button>
       </div>
       <textarea onfocusout="var self = window.getNote(this); self.text = this.value;" id="text" placeholder="Start Typing...">${this.text}</textarea>
-      `
-      section.innerHTML = sectionBody
-      document.querySelector('main').appendChild(section)
-    }
-    
-    this.removeNote = function(noteSection) {
-      if(window.confirm(`Are you sure you want to delete note: "${this.title}"`)) noteSection.remove()
-    }
-
-    this.render()
-    return this
+    `
+    section.innerHTML = sectionBody
+    if(props.color) section.children[0].style.backgroundColor = props.color
+    document.querySelector('main').appendChild(section)
+  }
+  
+  this.removeNote = function(noteSection) {
+    if(window.confirm(`Are you sure you want to delete note: "${this.title}"`)) noteSection.remove()
   }
 
-  new Note({id: `note-${noteCount}`})
+  this.render()
+  return this
+}
 
-  function newNote() {
-    noteCount++
-    new Note({id: `note-${noteCount}`})
+new Note({id: `note-${noteCount}`})
+
+function newNote() {
+  noteCount++
+  new Note({id: `note-${noteCount}`, color: getRandomColor()})
+}
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
